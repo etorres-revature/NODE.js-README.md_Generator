3cbb02521b2e1ea24edad37d425a25da6cb07f5d
Eric D. Torres <etorresnotary@gmail.com>
2020-09-15 15:31:34 -0500

    inquirer function completed

--- index.js
+++ index.js
@@ -1,3 +1,66 @@
 const inquirer = require("inquirer");
-const fs = require('fs');
+const fs = require("fs");
+
+inquirer
+  .prompt([
+    {
+      type: "input",
+      name: "project-title",
+      message: "What is the title of your project?",
+    },
+    {
+      type: "input",
+      name: "description",
+      message: "Please describe your project.",
+    },
+    {
+      type: "input",
+      name: "installation",
+      message: "Please provide specific instructions for the installation of your project.",
+    },
+    {
+      type: "input",
+      name: "usage",
+      message: "Provide information on how users can use this project.",
+    },
+    {
+      type: "input",
+      name: "contribution",
+      message: "What are the guidelines for making a contribution to this project?",
+    },
+    {
+      type: "input",
+      name: "test",
+      message: "What are the instructions for testing this project?",
+    },
+    {
+      type: "list",
+      message: "Which license are you choosing for your project?",
+      name: "license-type",
+      choices: [
+          "Apache License 2.0",
+          "MIT License", 
+          "BSD 2-Clause 'Simplified' License", 
+          "BSD 3-Clause 'New' or 'Revised' License",
+          "Boost Software License 1.0",
+          "Creative Commons Zero v1.0 Universal",
+          "Eclipse Public License 2.0", 
+          "GNU Affero General Public License v3.0", 
+          "GNU General Public License v2.0",
+          "GNU Lesser General Public License v2.1"
+      ]
+
+    }, 
+    {
+        type: "input", 
+        name: "github", 
+        message: "Please enter your GitHub username."
+    }, 
+    {
+        type: "input",
+        name: "email", 
+        message: "Please enter your e-mail address."
+    }
+  ])
+  .then()
 
