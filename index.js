d70f4e7e9e047e566f8aa7b429a079d59b42e8d9
Eric D. Torres <etorresnotary@gmail.com>
2020-11-20 15:23:11 -0600

    readme for PWA Budget Tracker

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const markDown = require("./utils/generateMarkDown");
 const path = require("path");
 
 //creating a director variable to push the generated README to the NODE.js Team Generator
-const OUTPUT_DIR = path.resolve("../React.js-Monsters_Rolodex");
+const OUTPUT_DIR = path.resolve("../PWA-Budget_Tracker");
 
 // array of questions for user
 const questions = [
--- utils/generateMarkDown.js
+++ utils/generateMarkDown.js
@@ -1,8 +1,10 @@
 //function to generate markdown code that will become the README file
 
-function generateMarkDown(data)  {
-    return `
-![Github License](https://img.shields.io/badge/License-${data.license.split(" ").join("_")}-brightgreen)
+function generateMarkDown(data) {
+  return `
+![Github License](https://img.shields.io/badge/License-${data.license
+    .split(" ")
+    .join("_")}-brightgreen)
 
 # ${data.title}
 
@@ -45,9 +47,37 @@ ${data.test}
 
 #### :memo: HTML5 :memo:
 
+**HTML5** is a markup language used for structuring and presenting content on the World Wide Web.  The goals are to improve the language with support for the latest multi-media and other new features; to keep the language both easily readable by humans and consistently understood by computers and devices; and to remain backward compatible to older software.  Many new symantec features are included.
+
+*HTML5* content borrowed from <a target="_blank" rel="noopener noreferrer">[this page](https://en.wikipedia.org/wiki/HTML5).</a>
+
+#### :art: CSS :art:
+
+**Cascading Style Sheets (CSS)** is a stylesheet language used for describing the presentation of a document written in a markup language (such as HTML5).  CSS is designed to enable the separation of presentation and content; including layout, colors, and fonts.  This separation improves content accessibility to provide more flexibility and control in the specification of presentation characteristics, enabling multiple web pages to share formatting by specifying relevant CSS in a separate file, which reduces complexity and repetition in the structural content (HTML), as well as enabling the file to be cached to improve the page load speed between the pages that share the file and its formatting.
+
+Separation of formatting and content also makes it feasible to present the same markup page in different styles for different rendering methods, such as on-screen, in print, by voice, and on Braille-based tactile devices. 
+
+*CSS* content borrowed from <a target="_blank" rel="noopener noreferrer">[this page](https://en.wikipedia.org/wiki/Cascading_Style_Sheets).</a>
+
+#### :shoe: Bootstrap 4 :shoe:
+
+**Bootstrap 4** is a free and open-source CSS framework directed at responsive, mobile-first front-end web development.  It contains CSS and (optionally) JavaScript-based design templates for typography, forms, buttons, navigation, and other interface components.  
+
+*Bootstrap 4* content borrowed from <a target="_blank" rel="noopener noreferrer">[this page](https://en.wikipedia.org/wiki/Bootstrap_(front-end_framework)).</a>
+
+#### :sparkler: JavaScript :sparkler:
+
 **JavaScript (JS)** is one of the core technologies of the World Wide Web (along with HTML and CSS). It enables interactive web pages and is an essential part of web applications.  JS is a multi-faceted, scripting language that provides versatility through Application Programming Interfaces (APIs) and Document Object Model (DOM) manipulation, among others.
 
 *JavaScript* content borrowed from <a target="_blank" rel="noopener noreferrer">[this page](https://en.wikipedia.org/wiki/JavaScript).</a>
+
+#### :chart_with_upwards_trend: Chart.js :chart_with_upwards_trend:
+
+**Chart.js** is a free and open source JavaScript library that makes data visualization fun and easy.  Use the library in conjunction with the HTML5 canvas tag to quickly add data visualization to your JavaScript projects. 
+
+Find out more about *Chart.js* at <a target="_blank" rel="noopener noreferrer">[this page](https://www.chartjs.org/docs/latest/).
+
+#### :globe_with_meridians: NODE.js :globe_with_meridians:
 
 **NODE.js** is an open-source, cross-platform JavaScript runtime environment that execute JavaScript code outside a web browser.  NODE.js lets developers use JavaScript to write command line tools and for server-side scripting.  NODE.js represents a "Javascript everywhere" paradigm, unifying web-application development around a single programming language, rather than different programming languages for server- and client-side scripts.  
 
@@ -61,11 +91,23 @@ ${data.test}
 
 *Express.js* content borrowed from <a target="_blank" rel="noopener noreferrer">[this page](https://expressjs.com/).</a>
 
-### :vertical_traffic_light: React.js :vertical_traffic_light:
+#### :japanese_ogre: MongoDB :japanese_ogre:
 
-**React.js** is an open-source, front-end, JavaScript library for building user interface components. React.js can be used as a base in the development of single-page or mobile applications.  It is only concerned with rendering data to the DOM.
+**MongoDB** is a document-oriented NoSQL database used for high volume data storage.  Instead of using tables and rows like traditional databases, MongoDB makes use of collections and documents.  Documents consist of key-value pairs that are the basic unit of data and more closely resemble the JSON architecture.    
 
-*React.js* content borrowed from <a target="_blank" rel="noopener noreferrer">[this page](https://en.wikipedia.org/wiki/React_(web_framework)#:~:text=React%20(also%20known%20as%20React,single%2Dpage%20or%20mobile%20applications.).</a>
+*MongoDB* content borrowed from <a target="_blank" rel="noopener noreferrer">[this page](https://www.guru99.com/what-is-mongodb.html#:~:text=MongoDB%20is%20a%20document%2Doriented,use%20of%20collections%20and%20documents.&text=Collections%20contain%20sets%20of%20documents,equivalent%20of%20relational%20database%20tables)</a>
+
+#### :school_satchel: Webpack :school_satchel:
+
+**Webpack** is a static module bundler for modern JavaScript applications.  When webpack process an application, it internally builds a dependency graph that maps every module the project needs and generates one or more bundles, which enables JavaScript to have greater modularization.
+
+*Webpack* content borrowed from <a target="_blank" rel="noopener noreferrer">[this page](https://webpack.js.org/concepts/)</a>
+
+#### :card_index: IndexedDB :card_index:
+
+**IndexedDB** is a way for developers to persistently store data inside a user's browser.  Because it lets you create web applications with rich query abilities regardless of network availability, your applications can work both online and offline.
+
+*IndexedDB* content borrowed from <a target="_blank" rel="noopener noreferrer">[this page](https://developer.mozilla.org/en-US/docs/Web/API/IndexedDB_API/Using_IndexedDB)</a>
 
 ### Author 
 
@@ -75,7 +117,9 @@ This *${data.title}* was built by :green_heart: ${data.developer} :green_heart:
 
 Check out my [GitHub profile](https://github.com/${data.github}).
 
-You can contact me by e-mail at ${data.email} for any additional questions and/ or clarifications you may need about the project.
+You can contact me by e-mail at ${
+    data.email
+  } for any additional questions and/ or clarifications you may need about the project.
 
 ##### License
 
@@ -85,6 +129,5 @@ You can contact me by e-mail at ${data.email} for any additional questions and/ 
 `;
 }
 
-
 //creating an export of this file so that other files have acces to it through "require"
-module.exports = generateMarkDown
+module.exports = generateMarkDown;
