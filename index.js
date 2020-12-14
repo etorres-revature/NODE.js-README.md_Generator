f94a94e5d4ede6ccafb9119b7b3405aefbcd0221
Eric D. Torres <etorresnotary@gmail.com>
2020-12-13 20:56:47 -0600

    portfolio readme

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const markDown = require("./utils/generateMarkDown");
 const path = require("path");
 
 //creating a director variable to push the generated README to the NODE.js Team Generator
-const OUTPUT_DIR = path.resolve("../user-directory");
+const OUTPUT_DIR = path.resolve("../portfolio");
 
 // array of questions for user
 const questions = [
